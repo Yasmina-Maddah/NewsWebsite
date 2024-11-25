4c704c3dd8603698bae33e18d54d2ac19c293327
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-25 19:14:23 +0200

    Added the api Routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ArticleController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Admin routes (requires admin authentication)
+Route::middleware(['auth:admin'])->group(function () {
+    Route::post('/news', [AdminController::class, 'createNews']);
+    Route::put('/news/{id}', [AdminController::class, 'editNews']);
+    Route::delete('/news/{id}', [AdminController::class, 'deleteNews']);
+});
+
+// User routes (requires user authentication)
+Route::middleware(['auth:web'])->group(function () {
+    Route::post('/articles', [ArticleController::class, 'createArticle']);
+    Route::delete('/articles/{id}', [ArticleController::class, 'deleteArticle']);
+    Route::get('/news', [NewsController::class, 'index']);
+    Route::get('/news/{id}', [NewsController::class, 'show']);
+    Route::get('/news/{id}/articles', [ArticleController::class, 'getArticlesForNews']);
+});
