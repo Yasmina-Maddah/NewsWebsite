47f96082b92a510385401f014cbaa6bbf2689f25
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-25 10:43:28 +0200

    Edited the Article.php Model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,7 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Article extends Model
-{
-    //
+class Article extends Model {
+    public function news() {
+        return $this->belongsTo(News::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
