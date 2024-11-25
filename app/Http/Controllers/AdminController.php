ac0dc4e9a79631bae866518e0f8553761e1cbd09
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-25 16:31:52 +0200

    Edite the Admin.php Model

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,7 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AdminController extends Controller
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Admin extends Authenticatable
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'email', 'password'];
+
+    public function news()
+    {
+        return $this->hasMany(News::class);
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
 }
--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Admin extends Authenticatable {
-    public function news() {
+class Admin extends Authenticatable
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'email', 'password'];
+
+    public function news()
+    {
         return $this->hasMany(News::class);
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
     }
 }
