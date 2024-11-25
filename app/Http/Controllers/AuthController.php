f6b35bc65af7ef3aa6c403d94a9f59095e152bf7
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-25 20:13:05 +0200

    Added the code for the Athentication Controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request, $role)
+    {
+        $credentials = $request->only(['email', 'password']);
+
+        $guard = $role === 'admin' ? 'admin' : 'api';
+
+        if (!$token = Auth::guard($guard)->attempt($credentials)) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::guard($guard)->factory()->getTTL() * 60,
+        ]);
+    }
+
+    public function logout(Request $request, $role)
+    {
+        $guard = $role === 'admin' ? 'admin' : 'api';
+
+        Auth::guard($guard)->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
 }
