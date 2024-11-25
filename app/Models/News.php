c6c5e61b40d7929611dbcc4a623d3077ee5ec98f
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-25 16:34:11 +0200

    Edited the News.php Model

--- app/Models/News.php
+++ app/Models/News.php
@@ -3,13 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class News extends Model {
-    public function admin() {
+class News extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'content', 'restricted_age', 'attachment', 'admin_id'];
+
+    public function admin()
+    {
         return $this->belongsTo(Admin::class);
     }
 
-    public function articles() {
+    public function articles()
+    {
         return $this->hasMany(Article::class);
+    }
+
+    public function getAttachmentAttribute($value)
+    {
+        return $value ? url('storage/' . $value) : null;
     }
 }
