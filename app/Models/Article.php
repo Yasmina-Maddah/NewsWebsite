e6dcd7e4de9d670085db06fa1f678fe4ce630640
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-25 16:34:54 +0200

    Edited the Article.php Model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,13 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Article extends Model {
-    public function news() {
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['content', 'news_id', 'user_id', 'attachment'];
+
+    public function news()
+    {
         return $this->belongsTo(News::class);
     }
 
-    public function user() {
+    public function user()
+    {
         return $this->belongsTo(User::class);
+    }
+
+    public function getAttachmentAttribute($value)
+    {
+        return $value ? url('storage/' . $value) : null;
     }
 }
