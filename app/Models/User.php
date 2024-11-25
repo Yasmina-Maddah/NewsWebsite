1fbfe70a0f64bc604ad03e3518e4c4fea7501352
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-25 16:33:37 +0200

    Edited the User.php Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,22 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class User extends Authenticatable {
-    public function articles() {
+class User extends Authenticatable
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'email', 'password'];
+
+    public function articles()
+    {
         return $this->hasMany(Article::class);
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
     }
 }
