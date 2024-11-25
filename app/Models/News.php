fe518368e8a89bf443b5ad91bd0794313f8d12b0
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-25 10:42:56 +0200

    Edited the News.php Model

--- app/Models/News.php
+++ app/Models/News.php
@@ -4,7 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class News extends Model
-{
-    //
+class News extends Model {
+    public function admin() {
+        return $this->belongsTo(Admin::class);
+    }
+
+    public function articles() {
+        return $this->hasMany(Article::class);
+    }
 }
