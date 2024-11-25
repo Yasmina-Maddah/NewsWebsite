d423066d9736ac4805aebc5f6754fab67cafc7ef
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-25 20:15:32 +0200

    Created the Users  Middleware

--- app/Http/Middleware/UserJWTMiddleware.php
+++ app/Http/Middleware/UserJWTMiddleware.php
@@ -3,18 +3,20 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserJWTMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle($request, Closure $next)
     {
-        return $next($request);
+        if (!$user = JWTAuth::parseToken()->authenticate()) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        if (Auth::guard('api')->check()) {
+            return $next($request);
+        }
+
+        return response()->json(['message' => 'Forbidden'], 403);
     }
 }
