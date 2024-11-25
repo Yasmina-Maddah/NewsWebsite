7a6d18b85fa12acb6e47068497f1bb035f66a6ce
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-25 16:49:26 +0200

    Added the News Controller

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\News;
 
 class NewsController extends Controller
 {
-    //
+    public function index()
+    {
+        $news = News::all(); 
+        return response()->json($news);
+    }
+
+    public function show($id)
+    {
+        $news = News::findOrFail($id); 
+        return response()->json($news);
+    }
 }
