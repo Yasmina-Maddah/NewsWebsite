4c2ec017f1de466a2969d33df2d2d3dc625d24be
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-25 10:35:45 +0200

    Created the Articles Table and added the Necessary Columns to it

--- database/migrations/2024_11_25_083002_create_articles_table.php
+++ database/migrations/2024_11_25_083002_create_articles_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->text('content');
+            $table->foreignId('news_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
