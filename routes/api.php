5614d1a1882b918ca10d424718fac7b5b510dfd8
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-25 20:24:34 +0200

    Edited the Routes

--- routes/api.php
+++ routes/api.php
@@ -1,28 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\ArticleController;
 
-// Admin routes (requires admin authentication)
-Route::middleware(['auth:admin'])->group(function () {
+Route::post('/login/{role}', [AuthController::class, 'login']);
+Route::post('/logout/{role}', [AuthController::class, 'logout']);
+
+Route::middleware(['jwt.admin'])->group(function () {
     Route::post('/news', [AdminController::class, 'createNews']);
     Route::put('/news/{id}', [AdminController::class, 'editNews']);
     Route::delete('/news/{id}', [AdminController::class, 'deleteNews']);
 });
 
-// User routes (requires user authentication)
-Route::middleware(['auth:web'])->group(function () {
+Route::middleware(['jwt.user'])->group(function () {
     Route::post('/articles', [ArticleController::class, 'createArticle']);
     Route::delete('/articles/{id}', [ArticleController::class, 'deleteArticle']);
     Route::get('/news', [NewsController::class, 'index']);
     Route::get('/news/{id}', [NewsController::class, 'show']);
     Route::get('/news/{id}/articles', [ArticleController::class, 'getArticlesForNews']);
 });
-    Route::middleware(['jwt.user'])->group(function () {
-        Route::post('/articles', [ArticleController::class, 'createArticle']);
-        Route::delete('/articles/{id}', [ArticleController::class, 'deleteArticle']);
-        Route::get('/news', [NewsController::class, 'index']);
-        Route::get('/news/{id}', [NewsController::class, 'show']);
-        Route::get('/news/{id}/articles', [ArticleController::class, 'getArticlesForNews']);
-}); 
